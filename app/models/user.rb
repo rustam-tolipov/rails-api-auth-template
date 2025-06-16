5d77ff3c7f1805b4b37f4ef4189f56b0f8ac5383
rustam-tolipov <69011963+rustam-tolipov@users.noreply.github.com>
2025-06-16 07:16:09 +0500

    ✨ Implement refresh token functionality with associated model, controller, and migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
 
+  has_many :refresh_tokens, dependent: :destroy
+
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, confirmation: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
 end
